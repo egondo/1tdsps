a79608c5d3c4400d1b035b7e2ad5753e2585db8e
Eduardo Gondo <egondo@gmail.com>
2025-02-10 22:26:13 -0300

    Segundo exercicio

--- string/caixa_alta.py
+++ string/caixa_alta.py
@@ -1,6 +1,15 @@
 def maiuscula(palavra: str) -> str:
     return palavra.upper()
 
+def cria_espaco(palavra: str) -> str:
+    resp = ""
+
+    for letra in palavra:
+        resp = resp + letra + " " # + indica concatenação em String 
+
+    return resp
+
 frase = "Hoje retornamos das férias"
-resp = maiuscula(frase)
+resp = cria_espaco(frase)
 print(resp)
+
