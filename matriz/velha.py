c815fb956a117dfe58faee18850962d2fe57ad8d
Eduardo Gondo <egondo@gmail.com>
2025-03-17 20:51:35 -0300

    Estrutura para o módulo velha.py

---
+++ matriz/velha.py
@@ -0,0 +1,18 @@
+def criaTabuleiro() -> list:
+    pass
+
+def haGanhador(mat: list) -> bool:
+    pass
+
+def temEspaco(mat: list) -> bool:
+    pass
+
+def joga(mat: list, lin: int, col: int, jogador: str) -> bool:
+    pass
+
+def trocaJogador(jogador:str) -> str:
+    pass 
+
+def imprime(mat: list):
+    for lin in mat:
+        print(lin)
